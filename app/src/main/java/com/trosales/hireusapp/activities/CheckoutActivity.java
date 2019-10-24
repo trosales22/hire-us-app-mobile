8eb74665a7a606dbbf7972079291c5a589456c50
Tristan Rosales <tristanrosales0@gmail.com>
2019-10-24 23:16:52 +0800

    Implement endpoint for payment process.

--- app/src/main/java/com/trosales/hireusapp/activities/CheckoutActivity.java
+++ app/src/main/java/com/trosales/hireusapp/activities/CheckoutActivity.java
@@ -1,10 +1,7 @@
 package com.trosales.hireusapp.activities;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.AppCompatButton;
@@ -19,23 +16,21 @@ import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
-import com.paypal.android.sdk.payments.PayPalAuthorization;
-import com.paypal.android.sdk.payments.PayPalConfiguration;
-import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
-import com.paypal.android.sdk.payments.PayPalPayment;
-import com.paypal.android.sdk.payments.PayPalService;
-import com.paypal.android.sdk.payments.PaymentActivity;
-import com.paypal.android.sdk.payments.PaymentConfirmation;
 import com.squareup.picasso.Picasso;
+import com.stripe.android.ApiResultCallback;
+import com.stripe.android.Stripe;
+import com.stripe.android.model.Card;
+import com.stripe.android.model.Token;
+import com.stripe.android.view.CardMultilineWidget;
 import com.trosales.hireusapp.R;
 import com.trosales.hireusapp.classes.commons.SharedPrefManager;
 import com.trosales.hireusapp.classes.constants.EndPoints;
 import com.trosales.hireusapp.classes.constants.Tags;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -51,17 +46,13 @@ public class CheckoutActivity extends AppCompatActivity {
     @BindView(R.id.lblBookingPreferredTime) TextView lblBookingPreferredTime;
     @BindView(R.id.lblBookingOtherDetails) TextView lblBookingOtherDetails;
     @BindView(R.id.btnCancelBooking) AppCompatButton btnCancelBooking;
+    @BindView(R.id.card_multiline_widget) CardMultilineWidget cardMultilineWidget;
     @BindView(R.id.btnPayUsingDebitOrCreditCard) AppCompatButton btnPayUsingDebitOrCreditCard;
 
     private String selectedDate, selectedTime, selectedTalentId, selectedPaymentOption;
     private Bundle bundle;
 
-    private static final String PAYPAL_KEY = "AZ9DrSKHmGSwUNTXAe5kTJCw3yDXWhUwMjUDevXJaP3-4khsDuNb3dS-x1E0tEhg0C2jHBFMQ8gb5rKs";
-    private static final int REQUEST_CODE_PAYMENT  = 1;
-    private static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
-    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
-    private static PayPalConfiguration payPalConfiguration;
-    private PayPalPayment payPalPayment;
+    private Stripe stripe;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -70,14 +61,14 @@ public class CheckoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_checkout);
         ButterKnife.bind(this);
 
+        stripe = new Stripe(getApplicationContext(), "pk_test_pHHixfchBGl7tkRAanAoe7TN00O13SSzVp");
+
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         bundle = getIntent().getExtras();
         selectedDate = Objects.requireNonNull(bundle).getString("temp_booking_date");
         selectedTime = Objects.requireNonNull(bundle).getString("temp_booking_time");
         selectedTalentId = Objects.requireNonNull(bundle).getString("temp_talent_id");
-
-        configPayPal();
 
         Picasso
                 .with(getApplicationContext())
@@ -109,6 +100,73 @@ public class CheckoutActivity extends AppCompatActivity {
             //payUsingDebitOrCreditCard();
             selectedPaymentOption = "DEBIT_CREDIT_CARD";
             //addToClientBookingList(getClientBookingParams());
+            final Card cardToSave = cardMultilineWidget.getCard();
+
+            if (cardToSave == null) {
+                Toast.makeText(this, "Invalid Card Data", Toast.LENGTH_SHORT).show();
+            }else{
+                stripe.createToken(cardToSave, new ApiResultCallback<Token>() {
+                    @Override
+                    public void onSuccess(Token token) {
+                        StringBuilder sbDescription = new StringBuilder();
+                        sbDescription
+                                .append(bundle.getString("talent_fullname"))
+                                .append(" (").append(bundle.getString("talent_category")).append(") ")
+                                .append(selectedDate).append(" (").append(selectedTime).append(")");
+
+                        AndroidNetworking.post(EndPoints.START_PAYMENT_URL)
+                                .addBodyParameter("stripe_token", token.getId())
+                                .addBodyParameter("amount", Objects.requireNonNull(bundle.getString("total_amount")).replace(",", ""))
+                                .addBodyParameter("description", sbDescription.toString())
+                                .setTag(Tags.CHECKOUT_ACTIVITY)
+                                .setPriority(Priority.MEDIUM)
+                                .build()
+                                .getAsJSONObject(new JSONObjectRequestListener() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            String status = response.getString("flag");
+                                            String msg = response.has("msg") ? response.getString("msg") : "";
+
+                                            if(status.equals("1")){
+                                                //finish();
+                                                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                                                //startActivity(new Intent(getApplicationContext(), CheckoutActivity.class));
+                                            }else{
+                                                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onError(ANError anError) {
+                                        String errorResponse = "\n\nCode: " +
+                                                anError.getErrorCode() +
+                                                "\nDetail: " +
+                                                anError.getErrorDetail() +
+                                                "\nBody: " +
+                                                anError.getErrorBody() +
+                                                "\nResponse: " +
+                                                anError.getResponse() +
+                                                "\nMessage: " +
+                                                anError.getMessage();
+
+                                        Log.e(Tags.CHECKOUT_ACTIVITY, errorResponse);
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onError(@NotNull Exception e) {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                e.getLocalizedMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
         });
     }
 
@@ -120,75 +178,6 @@ public class CheckoutActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void configPayPal(){
-        payPalConfiguration = new PayPalConfiguration()
-                .environment(CONFIG_ENVIRONMENT)
-                .clientId(PAYPAL_KEY)
-                .merchantName("PayPal Login")
-                .merchantPrivacyPolicyUri(Uri.parse("https://trosales.netlify.com/"))
-                .merchantUserAgreementUri(Uri.parse("https://trosales.netlify.com/"));
-    }
-
-    private void payUsingDebitOrCreditCard(){
-        Intent intent = new Intent(this, PayPalService.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, payPalConfiguration);
-        startService(intent);
-
-        payPalPayment = new PayPalPayment(
-                //new BigDecimal(Objects.requireNonNull(bundle.getString("total_amount")).replace(",","")),
-                new BigDecimal("1.00"),
-                "PHP",
-                "Payment",
-                PayPalPayment.PAYMENT_INTENT_ORDER
-        );
-
-        Intent intentPayment = new Intent(this, PaymentActivity.class);
-        intentPayment.putExtra(PaymentActivity.EXTRA_PAYMENT, payPalPayment);
-        intentPayment.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, payPalConfiguration);
-        startActivityForResult(intentPayment, REQUEST_CODE_PAYMENT);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == REQUEST_CODE_PAYMENT){
-            if(resultCode == Activity.RESULT_OK){
-                PaymentConfirmation paymentConfirmation = Objects.requireNonNull(data).getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
-                if(paymentConfirmation != null){
-                    try{
-                        Log.d("debug", paymentConfirmation.toJSONObject().toString(4));
-                        Log.d("debug", paymentConfirmation.getPayment().toJSONObject().toString(4));
-                        Toast.makeText(this, "Payment successful!", Toast.LENGTH_LONG).show();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }else if(resultCode == Activity.RESULT_CANCELED){
-                Toast.makeText(this, "Payment has been cancelled!", Toast.LENGTH_LONG).show();
-            }else if(resultCode == PaymentActivity.RESULT_EXTRAS_INVALID){
-                Toast.makeText(this, "Error occurred!", Toast.LENGTH_LONG).show();
-            }
-        }else if(requestCode == REQUEST_CODE_FUTURE_PAYMENT){
-            if(resultCode == Activity.RESULT_OK){
-                PayPalAuthorization payPalAuthorization = Objects.requireNonNull(data).getParcelableExtra(PayPalFuturePaymentActivity.EXTRA_RESULT_AUTHORIZATION);
-                if(payPalAuthorization != null){
-                    try{
-                        Log.d("debug", "FuturePaymentExample: " + payPalAuthorization.toJSONObject().toString(4));
-                        String authCode = payPalAuthorization.getAuthorizationCode();
-                        Log.d("debug", "Authorization Code: " + authCode);
-                    }catch(Exception ex){
-                        ex.getStackTrace();
-                    }
-                }
-            }else if(resultCode == Activity.RESULT_CANCELED){
-                Toast.makeText(this, "Payment has been cancelled!", Toast.LENGTH_LONG).show();
-            }else if(resultCode == PayPalFuturePaymentActivity.RESULT_EXTRAS_INVALID){
-                Toast.makeText(this, "Probably the attempt to previously start the PayPalService had an invalid PayPalConfiguration. Please see the docs.", Toast.LENGTH_LONG).show();
-            }
-        }
     }
 
     private HashMap<String, String> getClientBookingParams(){
--- app/src/main/java/com/trosales/hireusapp/classes/constants/EndPoints.java
+++ app/src/main/java/com/trosales/hireusapp/classes/constants/EndPoints.java
@@ -26,4 +26,5 @@ public class EndPoints {
     public static final String GET_ALL_CITY_MUNI_URL = API_URL + "client/get_city_muni_by_province_code";
     public static final String GET_ALL_CLIENT_BOOKED_URL = API_URL + "talents/get_all_client_booked";
     public static final String GET_RESERVED_SCHEDULE_OF_TALENT = API_URL + "talents/get_reserved_schedule_talent";
+    public static final String START_PAYMENT_URL = API_URL + "payment/start_payment";
 }
