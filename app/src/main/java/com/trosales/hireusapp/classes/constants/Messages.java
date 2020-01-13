fc0403b92e4e60870610dcde96a0325dadeaeeb0
Tristan Rosales <tristanrosales0@gmail.com>
2020-01-13 23:35:21 +0800

    Code refactor.

--- app/src/main/java/com/trosales/hireusapp/classes/constants/Messages.java
+++ app/src/main/java/com/trosales/hireusapp/classes/constants/Messages.java
@@ -9,7 +9,6 @@ public class Messages {
     public static final String LOGOUT_PROMPT = "Are you sure you want to logout?";
     public static final String OKAY_MSG = "Okay";
     public static final String CONFIRMATION_CAPTION = "Confirmation";
-    public static final String MSG_CAPTION = "MESSAGE";
     public static final String WARNING_MSG = "WARNING!";
     public static final String SHOW_PASSWORD_MSG = "Show Password";
     public static final String HIDE_PASSWORD_MSG = "Hide Password";
--- app/src/main/java/com/trosales/hireusapp/classes/constants/Tags.java
+++ app/src/main/java/com/trosales/hireusapp/classes/constants/Tags.java
@@ -4,11 +4,9 @@ public class Tags {
     public static final String LOGIN_ACTIVITY = "LoginActivity";
     public static final String MAIN_ACTIVITY = "MainActivity";
     public static final String TALENT_DETAILS_ACTIVITY = "TalentDetailsActivity";
-    public static final String UNAVAILABLE_DATES_FRAGMENT = "UnavailableDatesFragment";
     public static final String SET_BOOKING_DATE_AND_TIME_ACTIVITY = "SetBookingDetailsActivity";
     public static final String BOOKING_LIST_ACTIVITY = "BookingListActivity";
     public static final String FILTERING_ACTIVITY = "FilteringActivity";
     public static final String CHECKOUT_ACTIVITY = "CheckoutActivity";
     public static final String CLIENTS_ACTIVITY = "ClientsActivity";
-    public static final String ADD_REVIEWS_ACTIVITY = "AddReviewsActivity";
 }
